fdee0d4cc5cd9a8a04dd8acf083a9dfb0d3f6296
Chandrashekar <gcshekar08@gmail.com>
2018-11-15 13:20:59 +0530

    Update property_recommendation.py

--- Affine Analytics ML Challenge/property_recommendation.py
+++ Affine Analytics ML Challenge/property_recommendation.py
@@ -1,16 +1,9 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy.stats import spearmanr
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics.classification import f1_score
-from sklearn.ensemble import GradientBoostingClassifier
 
 def combine(df_to, df_from, df_combined):
     for i in df_to['id_accs']:
@@ -23,51 +16,6 @@ def con_bool(column, df):
         df[i] = df[i].replace(False, 0)
     return df
 
-'''
-df = pd.read_csv('Accounts_properties.csv', usecols=['id_accs', 'id_props'])
-print(len(df))
-df_1 = pd.read_csv('Accounts.csv')
-column = list(df_1.columns.values)
-df_train = pd.DataFrame(columns=column)
-for i in df['id_accs']:
-    df_train = df_train.append(df_1.loc[df_1['id_accs'] == i])
-
-            
-df_train['id_props'] = df['id_props']
-
-df_train.to_csv('train.csv', index=True,header=True,index_label='id_accs')
-
-
-df = pd.read_csv('Properties.csv', usecols=['id_props', 'sale_date__c'])
-df_2 = pd.read_csv('Accounts_properties.csv', usecols=['id_props'])
-
-df['sale_date__c'].fillna(0, inplace=True)
-a = []
-b = 0
-c = 0
-for i in df_2['id_props']:
-    b = df.loc[df['id_props'] == i]
-    b = b['sale_date__c'].values
-    print(b)
-    if not b:
-        c = 0
-        a.append(c)
-    else:
-        for j in b:
-            if j == 0:
-                c = -1
-                a.append(c)
-            else:
-                c = j[-4:]
-                a.append(int(c))
-    b = 0
-    print(c)
-
-print(a)
-print(len(a))
-df_train = pd.read_csv('train.csv')
-df_train['year'] = a
-df_train.to_csv('train_2.csv', index=False)'''
 
 df = pd.read_csv('train_3.csv')
 
@@ -108,10 +56,7 @@ print(df.describe())
 
 print(df.isnull().sum())
 
-target = df['id_props']
-
-#train = pd.concat([df.drop(['investor_type', 'id_props', 'id_accs'], axis=1),
-#                  pd.get_dummies(df['investor_type'])], axis=1)     
+target = df['id_props']   
 
 df.drop(['buyer_book', 'servicing_contract', 'cmbs', 'consultant', 'correspondent', 
          'foreign', 'master_servicer', 'lender_book', 'loan_sales_book', 
@@ -171,16 +116,7 @@ test['num_of_prperties_related_deals'] = test['number_of_properties'] * test['nu
 
 enc = LabelEncoder()    
 target = enc.fit_transform(target.values)
-print(target)
-
-'''
-for i in con_columns:
-    print(train[i].values)
-    train[i] = np.log(train[i].values)
-    print(test[i].values)
-    test[i] = np.log(test[i].values)'''
     
-
 model = RandomForestClassifier(n_estimators=10, criterion='entropy', max_features=None, random_state=7)
 model.fit(train, target)
 y = model.predict(test)
